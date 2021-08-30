dc09b6206bbacb2834b492b9f4c87bdb59dcec7d
seokjun jin <seokjun.jin@devsisters.com>
2021-08-31 08:41:37 +0900

    테스트 코드 작성

--- practice_01.py
+++ practice_01.py
@@ -1,2 +1,13 @@
-import byme
-byme.sign()
+balls = [1,2,3,4]
+weapons = [11,22,3,44]
+
+for ball_idx, ball_val in enumerate(balls):
+    print("ball", ball_val)
+    for weapon_idx, weapon_val in enumerate(weapons):
+        print("wepons", weapon_val)
+        if ball_val == weapon_val:
+            print("공과 무기가 충돌")
+            break
+    else:
+        continue
+    break
