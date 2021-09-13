7ea11b9bb8096326a008835117b959d1c6e26b41
seokjun jin <seokjun.jin@devsisters.com>
2021-09-13 09:19:30 +0900

    엑셀 자동화 연습

---
+++ Automation/test.py
@@ -0,0 +1,15 @@
+# list_num4 = [0,10,20,30,40,50,60,70,80,90]
+# # print(list_num4)
+# # print(list_num4[0:9:2])
+# # print(list_num4[:-5:2])
+
+# # print( 34 in list_num4)
+# # print( 30 in list_num4)
+
+# list_num4.append('대한민국')
+# print(list_num4)
+# print(type(list_num4))
+
+set_a = {0,1,2,3,4}
+set_b = {3,4,5,6,7}
+print(set_b - set_a)
